e34f6f6c62ef0cc45cba9729820da311529276ae
Mohammad Fatemi <mr.smf8@gmail.com>
2021-02-07 01:56:04 +0330

    feat: Add getParentID test Application

--- Makefile
+++ Makefile
@@ -181,6 +181,7 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
+	_getParentIDTest\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -253,6 +254,7 @@ EXTRA=\
 	printf.c umalloc.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
 	.gdbinit.tmpl gdbutil\
+	getParentIDTest.c
 
 dist:
 	rm -rf dist
---
+++ getParentIDTest.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(){
+    int pid = getpid();
+    printf(1, "parent id: %d\n", pid);
+
+    int child1 = fork();
+    if(child1 == 0){
+        printf(1, "child1 (%d) parent id: %d\n", getpid(),getparentid());
+        int child2 = fork();
+        if(child2 == 0){
+            printf(1, "child2 (%d) parent id: %d\n", getpid(), getparentid());
+        }else{
+            wait();
+            return 0;
+        }
+
+    }else{
+        wait();
+        return 0;
+    }
+}
