3a5b14eb298851f731292ea931a1ecaa3e43a940
Mohammad Fatemi <mr.smf8@gmail.com>
2021-02-07 23:48:39 +0330

    feat: Add round robin with custom quantum policy

--- trap.c
+++ trap.c
@@ -103,8 +103,11 @@ trap(struct trapframe *tf)
   // Force process to give up CPU on clock tick.
   // If interrupts were on while locks held, would need to check nlock.
   if(myproc() && myproc()->state == RUNNING &&
-     tf->trapno == T_IRQ0+IRQ_TIMER)
-    yield();
+     tf->trapno == T_IRQ0+IRQ_TIMER) {
+      if(ticks % QUANTUM == 0) {
+          yield();
+      }
+  }
 
   // Check if the process has been killed since we yielded
   if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
